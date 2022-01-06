9252bdcbfc4b1c970384bf2814dfbf0d79458fa5
LinQin <woshilinqin163@163.com>
2022-01-06 18:54:19 +0800

    feat: 学习vscode快捷键

--- 1.基本数据类型.py
+++ 1.基本数据类型.py
@@ -4,3 +4,40 @@ c = True
 d = '字符串'
 
 print(type(a), type(b), type(c), type(d))
+
+print(isinstance(a, int))
+
+
+class A:
+    pass
+
+
+class B(A):
+    pass  # isinstance() 会认为子类是一种父类类型
+
+
+print(isinstance(B(), A))  # True# type() 不会认为子类是一种父类类型
+print(type(A()) == A)  # True
+print(isinstance(A(), A))  # True
+print(type(B()) == A)  # Fale
+
+print(issubclass(bool, int))
+
+var1 = 11
+del var1
+
+5 + 4
+
+# 多个同时赋值
+a, b = 1, 2
+
+print('-----------------------------')  # 数组
+list = ['abc', 123, 'runoob', 10.2]
+tinylist = ['a123', 456]
+print(list)
+print(tinylist)
+print(list[0])
+print(list[1:3])
+print(list + tinylist)
+new_var = list * 2
+print(new_var)
--- README.md
+++ README.md
@@ -11,4 +11,5 @@ foo();
 
 ctrl + 方向键                   #单词粒度移动光标  
 Ctrl + Shift + \                #代码块的光标移动
-Ctrl + Shift + 方向键           #文本选择
+Ctrl + Shift + 上下键           #换行
+Ctrl + Alt + 上下键             #批量游标操作
