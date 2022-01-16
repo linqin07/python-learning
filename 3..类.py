530883bc8fa622f8d8d13a246d64be1ce124b46d
linqin <woshilinqin163@163.com>
2022-01-17 00:17:02 +0800

    feat: def

--- 3..类.py
+++ 3..类.py
@@ -6,15 +6,76 @@ class MyClass:
         print(self)
         print(self.__class__)
         print(id(self))
-        __age=18;
         pass
     def method(self):
-        print('__age=', self.__)
         return 'hello'
     
 x = MyClass()
 print('x.i', x.i)
 print('x.method', x.method())
+
+
+#类定义
+class people:
+    #定义基本属性
+    name = ''
+    age = 0
+    #定义私有属性,私有属性在类外部无法直接进行访问
+    __weight = 0
+    #定义构造方法
+    def __init__(self,name,age,weight):
+        self.name = name
+        self.age = age
+        self.__weight = weight
+    def speak(self):
+        print("%s 说: 我 %d 岁, %d kg。" %(self.name,self.age, self.__weight))
+ 
+# 实例化类
+p = people('美女', 24, 50)
+p.speak()
+print(p.name, p.age, '岁')
+
+class animal(people):
+    def speak(self):
+        return super().speak()
+    def run(self):
+        print('name run', self.name)
+
+animal = animal('动物', 10, 20)
+animal.speak()
+print(animal.name, animal.age)
+
+
+
+#可写函数说明,指定顺序.指定默认值
+def printinfo(age, name='qin'):
+   "打印任何传入的字符串"
+   print ("名字: ", name)
+   print ("年龄: ", age)
+   return 
+printinfo(age=50, name='lin')
+# print()
+printinfo(age=10)
+
+
+# 加了星号 * 的参数会以元组(tuple)的形式导入，存放所有未命名的变量参数。加了两个星号 ** 的参数会以字典的形式导入
+def printInfo(a, *b):
+    print('不定长输出:', type(b))
+    print('a=', a, ',', 'b')
+
+printInfo('a', 'b', 'c')
+
+def printInfo(a, **b):
+    print('不定长输出:', type(b))
+    print('a=', a, ',', b)
+    return
+printInfo('a', x=1, b=2)
+
+# python 使用 lambda 来创建匿名函数
+sum = lambda args1, args2 : args1 +args2; 
+print(sum(1,3))
+
+
 
 
 
---
+++ 4.其他.py
@@ -0,0 +1,6 @@
+# 列表推导式
+list = [1, 2, 3, 4, 5, 6]
+a = [x*2 for x in list ]
+print(a)
+
+https://www.runoob.com/python3/python3-data-structure.html
